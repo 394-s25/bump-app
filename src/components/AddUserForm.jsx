6b3c1de2a70907a83db879acb03ae8408bdc0744
annabellesole1 <annabellesole1@gmail.com>
2025-04-28 17:27:05 -0500

    Change text color in forms

--- src/components/AddUserForm.jsx
+++ src/components/AddUserForm.jsx
@@ -23,7 +23,7 @@ const AddUserForm = ({ onClose, onAddUser, user, playlistId }) => {
       position: 'fixed', top: 0, left: 0, right: 0, bottom: 0,
       backgroundColor: 'rgba(0,0,0,0.5)',
       display: 'flex', justifyContent: 'center', alignItems: 'center'
-    }}>
+    }} className='text-black'>
       <div style={{
         background: 'white', padding: '20px', borderRadius: '8px',
         width: '400px', maxWidth: '90%'
